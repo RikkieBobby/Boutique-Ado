e4170c604cd404ecbcf5fde542ef00b3cdae51c9
Richard Nolan <richienolan23@gmail.com>
2024-05-23 15:56:59 +0000

    add aws objects parameters

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -194,6 +194,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'boutiqueado-rn-9dd1f10dc111'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
